a88ecf43a50ada2b7c8783e6d4ebe564916be6b1
thejoin95 <miki.lombi@gmail.com>
2021-07-09 19:25:01 +0200

    add lotties and update footer

---
+++ pages/components/LottieAnimation/LottieAnimation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Lottie from 'react-lottie';
+
+export default function LottieAnimation(props) {
+  const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: props.animationData,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+      }
+    };
+  
+  return (
+    <div>
+      <Lottie 
+	    options={defaultOptions}
+        height={props.height}
+        width={props.width}
+      />
+    </div>
+  );
+}
