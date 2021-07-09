92e75b0d630bd79e7c9425651217b8a819f1eac8
thejoin95 <miki.lombi@gmail.com>
2021-07-09 23:06:00 +0200

    split

--- pages/components/Footer/footer.js
+++ pages/components/Footer/footer.js
@@ -9,44 +9,7 @@ import {
     VisuallyHidden,
 } from '@chakra-ui/react';
 import { FaFacebook, FaGithub, FaHeart, FaTwitter } from 'react-icons/fa';
-
-const Logo = () => {
-    return (
-        <Image 
-            borderRadius="full"
-            boxSize="32px"
-            src="/images/logo.png"
-            alt="Message in a bottle Logo"
-        />
-    );
-};
-
-const SocialButton = ({
-    children,
-    label,
-    href,
-}) => {
-    return (
-        <chakra.button
-            bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-            rounded={'full'}
-            w={8}
-            h={8}
-            cursor={'pointer'}
-            as={'a'}
-            href={href}
-            display={'inline-flex'}
-            alignItems={'center'}
-            justifyContent={'center'}
-            transition={'background 0.3s ease'}
-            _hover={{
-                bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-            }}>
-            <VisuallyHidden>{label}</VisuallyHidden>
-            {children}
-        </chakra.button>
-    );
-};
+import SocialButton from './SocialButton';
 
 const Footer = () => {
     const url = process.env.URL || 'https://www.ilbottiglionediciro.it';
@@ -62,7 +25,13 @@ const Footer = () => {
                 spacing={3}
                 justify={{ base: 'center', md: 'space-between' }}
                 align={{ base: 'center', md: 'center' }}>
-                <Logo />
+
+                <Image 
+                    borderRadius="full"
+                    boxSize="32px"
+                    src="/images/logo.png"
+                    alt="Message in a bottle Logo"
+                />
                 <Text>Made with <FaHeart style={{display: 'inline'}} color='var(--chakra-colors-red-400)' /> in Florence</Text>
                 <Stack direction={'row'} spacing={3}>
                     <SocialButton label={'Share on TW'} href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(sharerMessage)}`}>
