0a89884ea7df688519202ef70d3661db0e15af07
thejoin95 <miki.lombi@gmail.com>
2021-07-09 19:25:28 +0200

    update index and add env

--- pages/index.js
+++ pages/index.js
@@ -6,54 +6,81 @@ import {
   Text,
   Button,
   Icon,
-  IconProps,
 } from '@chakra-ui/react';
-import Footer from "./components/Footer/footer";
+import Head from 'next/Head';
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
-    <Container maxW={'5xl'}>
-      <Stack
-        textAlign={'center'}
-        align={'center'}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}>
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}>
-          Meeting scheduling{' '}
-          <Text as={'span'} color={'orange.400'}>
-            made easy
+    <>
+      <Head>
+        <title>Il Bottiglione di Ciro - 56K</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content='Inspired by the Netflix series "Generation 56K" by The Jackal, you can send a message to anyone with a delay in opening. Like the Bottiglione di Ciro' />
+        <meta name="author" content="@thejoin95" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#5bbad5" />
+    
+        <meta property="og:title" content="Il Bottiglione di Ciro - 56K" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={`${process.env.URL}`} />
+        <meta property="og:site_name" content={`${process.env.DOMAIN}`} />
+        <meta property="og:description" content='Inspired by the Netflix series "Generation 56K" by The Jackal, you can send a message to anyone with a delay in opening. Like the Bottiglione di Ciro'/>
+
+        <meta name="twitter:widgets:csp" content="on" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:domain" content={`${process.env.DOMAIN}`} />
+        <meta name="twitter:title" content="Il Bottiglione di Ciro - 56K" />
+        <meta name="twitter:description" content='Send a message to anyone with a delay in opening. Like the Bottiglione di Ciro in #generation56k #netflix' />
+      </Head>
+      <Container maxW={'5xl'}>
+        <Stack
+          textAlign={'center'}
+          align={'center'}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Meeting scheduling{' '}
+            <Text as={'span'} color={'orange.400'}>
+              made easy
+            </Text>
+          </Heading>
+          <Text color={'gray.500'} maxW={'3xl'}>
+            Never miss a meeting. Never be late for one too. Keep track of your
+            meetings and receive smart reminders in appropriate times. Read your
+            smart “Daily Agenda” every morning.
           </Text>
-        </Heading>
-        <Text color={'gray.500'} maxW={'3xl'}>
-          Never miss a meeting. Never be late for one too. Keep track of your
-          meetings and receive smart reminders in appropriate times. Read your
-          smart “Daily Agenda” every morning.
-        </Text>
-        <Stack spacing={6} direction={'row'}>
-          <Button
-            rounded={'full'}
-            px={6}
-            colorScheme={'orange'}
-            bg={'orange.400'}
-            _hover={{ bg: 'orange.500' }}>
-            Get started
-          </Button>
-          <Button rounded={'full'} px={6}>
-            Learn more
-          </Button>
+          <Stack spacing={6} direction={'row'}>
+            <Button
+              rounded={'full'}
+              px={6}
+              colorScheme={'orange'}
+              bg={'orange.400'}
+              _hover={{ bg: 'orange.500' }}>
+              Get started
+            </Button>
+            <Button rounded={'full'} px={6}>
+              Learn more
+            </Button>
+          </Stack>
+          <Flex w={'full'}>
+            <Illustration
+              height={{ sm: '24rem', lg: '28rem' }}
+              mt={{ base: 12, sm: 16 }}
+            />
+          </Flex>
         </Stack>
-        <Flex w={'full'}>
-          <Illustration
-            height={{ sm: '24rem', lg: '28rem' }}
-            mt={{ base: 12, sm: 16 }}
-          />
-        </Flex>
-      </Stack>
+      </Container>
       <Footer />
-    </Container>
+    </>
   );
 }
 
